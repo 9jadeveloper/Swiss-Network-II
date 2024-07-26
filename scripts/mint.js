0c65faf17866476dbc8b7689920aa5f892c5fe01
Ibrahim Olanrewaju <iolawale46@yahoo.com>
2024-07-26 01:35:55 +0000

    Initial commit

---
+++ scripts/mint.js
@@ -0,0 +1,37 @@
+const hre = require("hardhat");
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0xcF6A06E83F6925E1EE8B8628D63A3EAA62463e53";
+  const [signer] = await hre.ethers.getSigners();
+
+  const contractFactory = await hre.ethers.getContractFactory("TestToken");
+  const contract = contractFactory.attach(contractAddress);
+
+  const functionName = "mint100tokens";
+  const mint100TokensTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName),
+    0
+  );
+
+  await mint100TokensTx.wait();
+
+  console.log("Transaction Receipt: ", mint100TokensTx.hash);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
